239339c034f0b05df38e90d185c95d873019a2d0
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-09-15 06:50:33 +0000

    codespace committed

---
+++ src/_25_09_15_Calculator/Calculator.java
@@ -0,0 +1,14 @@
+package _25_09_15_Calculator;
+
+public class Calculator {
+    public static void calculate(){
+        int choice;
+
+        System.out.println(" Please choose a calculation process:");
+        System.out.println("1-Sum/n2-Subtract/n3-Multiple/n4-Divide/n5- Power/n6-RootSquare/n7-türev/n8-integral/n9-mode/n10-mutlak değer:");
+//choice = Helper.scanInput();
+switch(choice){
+case 1:
+System.out.println("a");
+    }}
+}
