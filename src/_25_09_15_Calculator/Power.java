841a0ae17f1a36b34969be6e2f7422dd33041aa3
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-09-15 07:06:04 +0000

    files added

---
+++ src/_25_09_15_Calculator/Power.java
@@ -0,0 +1,11 @@
+package _25_09_15_Calculator;
+
+public class Power {
+    public static int power(int a, int b ){
+        int result=1;
+        for (int i=1; i<b;i++){
+result*=a;
+        }
+return result;
+    }
+}
