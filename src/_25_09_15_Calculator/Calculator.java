d1773f4f2b47103a8982dc61fb8b6599950de72d
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-09-15 10:26:08 +0300

    Update Calculator.java

--- src/_25_09_15_Calculator/Calculator.java
+++ src/_25_09_15_Calculator/Calculator.java
@@ -6,9 +6,30 @@ public class Calculator {
 
         System.out.println(" Please choose a calculation process:");
         System.out.println("1-Sum\n2-Subtract\n3-Multiple\n4-Divide\n5- Power\n6-RootSquare\n7-türev\n8-integral\n9-mode\n10-mutlak değer:");
-//choice = Helper.scanInput();
+choice = Helper.scanInput();
 switch(choice){
 case 1:
+s
+break;
+case 2:
+break;
+case 3:
+break;
+case 4:
+break;
+case 5:
+break;
+case 6:
+break;
+case 7:
+break;
+case 8:
+break;
+case 9:
+break;
+case 10:
+break;
+
 System.out.println("a");
     }}
 }
